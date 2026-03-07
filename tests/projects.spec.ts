6c35e9bd65d0d2adfaa365a5c2f82437741d60ee
vaghelabhavikb <103305324+vaghelabhavikb@users.noreply.github.com>
2026-03-07 18:49:00 +0530

    Fixture implementation completed (#4)
    Co-authored-by: vaghelabhavikb1 <vaghelabhavikb1@gmail.com>

--- tests/projects.spec.ts
+++ tests/projects.spec.ts
@@ -1,56 +1,34 @@
-import { test } from '@playwright/test'
+// import { test } from '@playwright/test'
+import { test } from './fixtures'
 import createByNameJson from '../test-data/Projects/CreateProjectsByName.json'
 import createWithOpFieldsJson from '../test-data/Projects/CreateProjectsWithOptionalFields.json'
 import createIssuesInProjectJson from '../test-data/Projects/CreateIssuesInProject.json'
-import { LandingPage } from '../pom/landing-page'
-import { ProjectsQueryPage } from '../pom/projects-query-page'
-import { CreateProjectForm } from '../pom/create-project-form'
 import { ProjectCreationFields } from '../pom/create-project-form'
 import { IssueCreationFields } from '../pom/create-issue-form'
-import { ProjectPage } from '../pom/project-page'
-import { CreateIssueform } from '../pom/create-issue-form'
-import { IssueInfo } from '../pom/issue-info'
-import { ProjectIssues } from '../pom/project-issues'
 
 test.describe('Projects tests', () => {
 
-    let landingPage: LandingPage
-    let projectsQueryPage: ProjectsQueryPage
-    let createProjectForm: CreateProjectForm
-    let projectPage: ProjectPage
-    let createIssueForm: CreateIssueform
-    let issueInfo: IssueInfo
-    let projectIssues: ProjectIssues
-
-    test.beforeEach(async ({ page }) => {
-        landingPage = new LandingPage(page)
-        projectsQueryPage = new ProjectsQueryPage(page)
-        createProjectForm = new CreateProjectForm(page)
-        projectPage = new ProjectPage(page)
-        createIssueForm = new CreateIssueform(page)
-        issueInfo = new IssueInfo(page)
-        projectIssues = new ProjectIssues(page)
-
+    test.beforeEach(async ({ landingPage, page }) => {
         await page.goto('')
         await landingPage.navToProjectsModule()
     })
 
     createByNameJson.names.forEach((name: string) => {
-        test(`Create Project with name: ${name}`, async () => {
+        test(`Create Project with name: ${name}`, async ({ projectsQueryPage, createProjectForm }) => {
             await projectsQueryPage.launchCreateProjectForm()
             await createProjectForm.createProjectWithNameOnly(name)
         })
     })
 
     createWithOpFieldsJson.Projects.forEach((fieldValues: ProjectCreationFields) => {
-        test(`Create Project with optional fields: ${fieldValues.ProjectName}`, async () => {
+        test(`Create Project with optional fields: ${fieldValues.ProjectName}`, async ({ projectsQueryPage, createProjectForm }) => {
             await projectsQueryPage.launchCreateProjectForm()
             await createProjectForm.createProjWithOptionalFields(fieldValues)
         })
     })
 
     createIssuesInProjectJson.issues.forEach((fieldValues: IssueCreationFields) => {
-        test(`Create Issue in Project: ${fieldValues.Subject}`, { tag: "@IP" }, async () => {
+        test(`Create Issue in Project: ${fieldValues.Subject}`, { tag: "@IP" }, async ({ projectsQueryPage, projectPage, projectIssues, createIssueForm, issueInfo }) => {
             await projectsQueryPage.launchDocIDProject()
             await projectPage.launchIssueTab()
             await projectIssues.launchCreateIssueForm()
