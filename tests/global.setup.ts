e945a95580290e32a5a37435b2df67565573044e
vaghelabhavikb <103305324+vaghelabhavikb@users.noreply.github.com>
2026-03-11 15:56:12 +0530

    Parallel execution completed. Some minor refactoring (#10)
    Co-authored-by: vaghelabhavikb1 <vaghelabhavikb1@gmail.com>

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -3,23 +3,33 @@ import { chromium, FullConfig, expect } from '@playwright/test';
 import { LoginPage } from '../pom/login-page';
 
 async function globalSetup(config: FullConfig) {
-    // const baseURLFromConfig = config.projects[0].use.baseURL;
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    var loginPage = new LoginPage(page)
-    // const creds = TestDataUtil.getCredentials('validUser')
+    const chromeSetup = async () => {
+        const browser = await chromium.launch({ channel: 'chrome', headless: false });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        var loginPage = new LoginPage(page)
+        await page.goto('http://localhost:3000')
+        await loginPage.navToLoginPage()
+        await loginPage.loginToApp('admin', 'localadmin')
+        await expect(page).toHaveTitle(/Redmine/i)
+        await context.storageState({ path: './auth/storageState.json' });
+        await browser.close();
+    }
 
-    await page.goto('http://localhost:3000')
-    await loginPage.navToLoginPage()
-    await loginPage.loginToApp('admin', 'localadmin')
-    // await loginPage.(creds.username, creds.password)
-    await expect(page).toHaveTitle(/Redmine/i)
-
-    // Save authenticated state
-    await context.storageState({ path: './auth/storageState.json' });
-
-    await browser.close();
+    const edgeSetup = async () => {
+        const edgeBrowser = await chromium.launch({ channel: 'msedge', headless: false });
+        const edgeContext = await edgeBrowser.newContext();
+        const edgePage = await edgeContext.newPage();
+        var loginPage = new LoginPage(edgePage)
+        await edgePage.goto('http://localhost:3001')
+        await loginPage.navToLoginPage()
+        await loginPage.loginToApp('admin', 'localadmin')
+        await expect(edgePage).toHaveTitle(/Redmine/i)
+        await edgeContext.storageState({ path: './auth/edge-storageState.json' });
+        await edgeBrowser.close();
+    }
+    await Promise.all([chromeSetup(), edgeSetup()]);
+    // await Promise.all([chromeSetup()]);
 }
 
 export default globalSetup;
